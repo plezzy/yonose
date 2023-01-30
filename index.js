7ac6cc557aa877a9babb0fb1d6a7193726e40fbe
Juan Pablo <bendox07@gmail.com>
2023-01-30 11:49:20 -0500

    Version 0.5 - Re animation for FAQs and fixing nav__link block

--- index.js
+++ index.js
@@ -1,53 +1,61 @@
-(function()
+const list = document.querySelector('.nav__list')
+
+const menu = document.querySelector('.header__menu')
+
+const icon = document.querySelector('.menu__item-icon')
+
+
+const faqs = document.querySelector('.faqs')
+
+const faqsStyles = window.getComputedStyle(faqs)
+
+let faqsHeight = faqsStyles.getPropertyValue("height")
+
+console.log(faqsHeight)
+
+const faqsLink = document.querySelector('.info__faqs-icon')
+
+const button = document.querySelector('.faqs__button-text')
+
+
+menu.addEventListener
+(
+    'click', 
+    () => 
+    {
+        list.classList.toggle('nav__list--show')
+        icon.classList.toggle('menu__item-icon--show')
+    }
+)
+
+faqsLink.addEventListener('click', show)
+
+function show()
 {
-    const list = document.querySelector('.nav__list')
+    faqs.classList.add('faqs--show')
 
-    const menu = document.querySelector('.header__menu')
+    setTimeout(() => 
+    {
+        faqs.style.height = "auto"
+        faqs.classList.remove("faqs--show")
 
-    const icon = document.querySelector('.menu__item-icon')
+        console.log("function show - actived")
+        console.log(faqsHeight)
+    }
+    , 699)
+}
 
-    
-    const faqs = document.querySelector('.faqs')
-    
-    const faqsLink = document.querySelector('.info__faqs-icon')
-    
-    const button = document.querySelector('.faqs__button-text')
+ button.addEventListener
+ (
+     'click', 
+     () =>
+     {
+         faqs.classList.add('faqs--hide')
 
-
-    menu.addEventListener
-    (
-        'click', 
-        () => 
-        {
-            list.classList.toggle('nav__list--show')
-            icon.classList.toggle('menu__item-icon--show')
-        }
-    )
-
-    faqsLink.addEventListener('click', 
-        () =>   
-        {
-            // faqs.classList.toggle('faqs__show')
-            faqs.style.height = "auto"
-            faqs.style.transform = "scale(1)"
-            // console.log(click)
-        }
-    )
-
-    button.addEventListener
-    (
-        'click', 
-        () =>
-        {
-            faqs.style.transform = "scale(0)"
-            setTimeout
-            (
-                () => 
-                {
-                    faqs.style.height = "0px"
-                }
-                , "500"
-            )
-        })
-
-})();
+         setTimeout(() => 
+         {
+             faqs.style.height = "0px"
+             faqs.classList.remove("faqs--hide")
+         }
+         , 1200)
+     })
